444ca0144177ff8c2041d3af0cc191ff6a6dd444
Vasco Gaitinha <vascomiguel.gaitinha@gmail.com>
2023-12-19 17:27:15 +0000

    Edited user routes and model for solved quizz array

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -27,6 +27,7 @@ router.get('/users/:userId', (req,res)=>{
 })
 
 router.put('/users/:userId', async (req, res) => {
+
     try {
         const { userId } = req.params;
         const updatedData = req.body;
@@ -43,4 +44,29 @@ router.put('/users/:userId', async (req, res) => {
     }
 });
 
+router.put('/users/:userId', async (req, res) => {
+
+    const userId = req.params.userId;
+    const quizzId = req.body.quizzId;
+
+    try {
+ 
+        const updatedUser = await Team.findByIdAndUpdate(
+            userId,
+            { $push: { sovledQuizz: quizzId } },
+            { new: true })
+        
+        if (!updatedUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json(req.body);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 module.exports = router; 
+
+
